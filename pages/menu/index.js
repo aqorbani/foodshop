2f52b69b4f0b142668506d935b8e233fb5abec65
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-14 16:16:56 +0330

    create menu and fetch data from json server

---
+++ pages/menu/index.js
@@ -0,0 +1,15 @@
+import MenuPage from "@/components/templates/MenuPage";
+
+export default function Menu({ data }) {
+  return <MenuPage data={data} />;
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${process.env.BASE_URL}/data`);
+  const data = await res.json();
+
+  return {
+    props: { data },
+    revalidate: +process.env.REVALIDATE, //seconds
+  };
+}
